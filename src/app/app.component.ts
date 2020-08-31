aa2973457f33b5ce18a5e52a40462b1cafbc21d8
rohan jaiswal <rohanjaiswal372@gmail.com>
2020-08-31 13:21:42 -0400

    updated content and contanct form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+  contactForm: FormGroup;
+  onSuccess = false;
+  onError = false
+
+  ngOnInit() {
+    this.contactForm = new FormGroup({
+      name: new FormControl(null),
+      company: new FormControl(null),
+      email: new FormControl(null),
+      message: new FormControl(null),
+    });
+  }
+
+  onSubmit(){
+    //do something
+    console.log(this.contactForm)
+    //http post request
+    
+    //if success
+    if(true){
+      this.onSuccess = true;
+    }
+    else{
+      this.onError = true;
+    }
+
+    setTimeout(()=>{                           
+      this.onSuccess = false;
+      this.onError = false;
+    }, 5000);
+
+  }
 }
